a3acd583b3f2dad9057795c9bb1e0ef97ffa60fb
leesc22 <leesiewching22@gmail.com>
2017-09-13 17:22:55 +0800

    set up validations and shorten methods at url model

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,10 @@
 class Url < ApplicationRecord
+	validates :long_url, presence: true
+	validates :long_url, format: { with: URI.regexp }, allow_blank: true
+	validates :short_url, uniqueness: true
+  before_save :shorten_url
+
+	def shorten_url
+		self.short_url = rand(36**7).to_s(36)
+	end
 end
