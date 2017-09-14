559069b989e7cf7589bedf727a8098e6eaea6b4b
leesc22 <leesiewching22@gmail.com>
2017-09-14 10:35:12 +0800

    set up destroy for url

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -22,6 +22,14 @@ class UrlsController < ApplicationController
 		@url = Url.find(params[:id])
 	end
 
+	def destroy
+		@url = Url.find(params[:id])
+		@url.destroy
+		redirect_to urls_path
+	end
+
+	private
+
 	def url_params
 		params.require(:url).permit(:long_url)
 	end
