709afb23fe7ec4825db2104d2584947cb4ac393e
leesc22 <leesiewching22@gmail.com>
2017-09-14 10:28:26 +0800

    set up create for url

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -8,7 +8,22 @@ class UrlsController < ApplicationController
 		@url = Url.new
 	end
 
+	def create
+		@url = Url.new(url_params)
+		@url.shorten
+		if @url.save
+			redirect_to @url
+		else
+			render 'new'
+		end
+	end
+
 	def show
 		@url = Url.find(params[:id])
 	end
+
+	def url_params
+		params.require(:url).permit(:long_url)
+	end
 end
+
--- app/models/url.rb
+++ app/models/url.rb
@@ -2,9 +2,8 @@ class Url < ApplicationRecord
 	validates :long_url, presence: true
 	validates :long_url, format: { with: URI.regexp }, allow_blank: true
 	validates :short_url, uniqueness: true
-  before_save :shorten_url
 
-	def shorten_url
+	def shorten
 		self.short_url = rand(36**7).to_s(36)
 	end
 end
