54582204cbd21024806eb0bcabc5623da0b803b2
leesc <leesiewching22@gmail.com>
2021-10-01 20:53:30 +0800

    clone, bundle install and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170913024831) do
+ActiveRecord::Schema.define(version: 2017_09_13_024831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "urls", force: :cascade do |t|
-    t.string   "long_url"
-    t.string   "short_url"
+  create_table "urls", id: :serial, force: :cascade do |t|
+    t.string "long_url"
+    t.string "short_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
